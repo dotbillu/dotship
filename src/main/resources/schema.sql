b6df39ef701d521894600f879e4a22fd3aaaa2dc
dotbillu <abhay@dotbillu.in>
2026-08-30 13:59:09 +0530

    intial

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    avatar_url VARCHAR, 
+    email VARCHAR(255) NOT NULL UNIQUE,
+    google_id VARCHAR UNIQUE,
+    password VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE product (
+id UUID PRIMARY KEY DEFAULT gen_random_uuid()
+);
